0a8c5291c405ff2980e422b2c71a486119d0b357
Mercy <mercyoladeind@gmail.com>
2022-09-08 08:04:02 -0700

    Fixed errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
- puts("Programming is like building a multilingual puzzle \n ");
- getchar();
- return (0);
+puts("Programming is like building a multilingual puzzle \n ");
+getchar();
+return (0);
 }
